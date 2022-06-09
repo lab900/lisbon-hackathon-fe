f22dc1861cc27a04b54e8b4ae1b1c948a1e0ae09
lab900-winand-vandenbergh <winand.vandenbergh@lab900.com>
2022-06-09 19:25:51 +0100

    Added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 interface Layer {
   id: string;
@@ -25,6 +25,21 @@ export class AppComponent {
   public config$: Observable<Config>;
 
   public constructor(private http: HttpClient) {
-    this.config$ = this.http.get<Config>('assets/sections.json');
+    this.config$ = this.http.get<Config>('assets/sections.json').pipe(
+      tap(() => setTimeout(() => this.initScrollMagic()))
+    );
+  }
+
+  private initScrollMagic(): void {
+    const controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({
+      triggerElement: '#reveal1',
+      triggerHook: 0.9, // show, when scrolled 10% into view
+      duration: '100%', // hide 10% before exiting view (80% + 10% from bottom)
+      offset: 50 // move trigger to center of element
+    })
+      .setClassToggle('#reveal1', 'visible') // add class to reveal
+      .addTo(controller);
   }
 }
