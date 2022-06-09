ca9a9f2ae6a7b50af26573abed2f8036b2bedf27
Frederico <frederico.teixeira@lab900.com>
2022-06-09 20:14:07 +0200

    make scrollmagic lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as ScrollMagic from 'scrollmagic';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   title = 'hackathon';
-
-
 }
